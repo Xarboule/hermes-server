aca02161af67c17ed9be38c010b3c775aa248bb3
Remi Dulong <remi.dulong@telecom-sudparis.eu>
2017-02-03 12:12:29 +0100

    tentative POST

--- node/server.js
+++ node/server.js
@@ -1,33 +1,80 @@
 var http = require('http');
 var bodyParser = require('body-parser');
 var express = require('express');
+var net = require('net');
+
 var app = express();
 
-/** bodyParser.urlencoded(options)
- * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
- * and exposes the resulting object (containing the keys and values) on req.body
- */
+
+var ip = '';
+var port = 56987;
+var client = new net.Socket();
+
+
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
- */
 app.use(bodyParser.json());
 
-app.post("/", function (req, res) {
-    console.log('IP du robot : ');
-    console.log(req.body.ip);
-    res.redirect('/remote');
-});
 
-app.get('/', function(req, res){
+// Routes :
+
+app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
 app.get('/remote', function(req, res) {
     res.render('remote.ejs');
+});
+
+app.post("/", function (req, res) {     // Envoi du formulaire (ip du robot)
+    console.log('IP du robot : '+req.body.ip);
+
+    if(req.body.ip !== "debug")
+    {
+        client.connect(port, ip, function() {
+            console.log('Connected to '+ip);
+        });
+    }
+
+    res.redirect('/remote');
+});
+
+
+// Events :
+
+app.post('/remote', function (req, res) {
+    if(req.body.orderstr === "go"){
+        console.log('Le robot avance !');
+    }
+    else if(req.body.orderstr === "gor"){
+        console.log('Le robot recule !');
+    }
+    else if(req.body.orderstr === "stop"){
+        console.log("Le robot s'arrete !");
+    }
+    else if(req.body.orderstr === "sweepL"){
+        console.log('Le robot tourne a gauche !');
+    }
+    else if(req.body.orderstr === "sweepR"){
+        console.log('Le robot tourne a droite !');
+    }
+    else if(req.body.orderstr ==="sweepstop"){
+        console.log('Le robot arrete de tourner !');
+    }
+    else {
+        console.log('Ordre inconnu '+req.body.orderstr );
+    }
+
+})
+
+
+
+
+
+app.on('close', function(req, res){
+    console.log('Fermeture du client.');
 });
 
 app.listen(8080);
