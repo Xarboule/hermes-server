20ecf5cb2800e4edddf94e5fea0f79a0e220a59e
remi.dulong <remi.dulong@telecom-sudparis.eu>
2017-03-23 11:37:00 +0000

    snmp ok

--- server/snmp.js
+++ server/snmp.js
@@ -8,13 +8,13 @@ var snmp = require('net-snmp');
 var options = {
     port: 8079,
     retries: 0,
-    timeout: 1000,
+    timeout: 3000,
     transport: "udp4",
     trapPort: 162,
     version: snmp.Version1
 };
 
-var session = snmp.createSession ("137.74.162.172", "public", options);
+var session = snmp.createSession ("192.168.43.243", "public", options);
 
 var oids = ["1.3.6.1.2.1.1.1.0"];
 
@@ -24,7 +24,7 @@ session.get (oids, function (error, varbinds) {
     } else {
         for (var i = 0; i < varbinds.length; i++)
             if (snmp.isVarbindError (varbinds[i]))
-                console.error ("SNMP : " + snmp.varbindError (varbinds[i]))
+                console.error ("SNMP : " + snmp.varbindError (varbinds[i]));
             else
                 console.log ("SNMP : " + varbinds[i].oid + " = " + varbinds[i].value);
     }
