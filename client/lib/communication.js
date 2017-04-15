64c327b9bc2b04404575466dd8e8985676c5730a
Remi Dulong <remi.dulong@telecom-sudparis.eu>
2017-04-16 01:58:36 +0200

    correction

--- client/lib/communication.js
+++ client/lib/communication.js
@@ -31,7 +31,7 @@ socket.on('message', function(e) {
 });
 
 function refreshStatus(status){
-    console.log(JSON.stringify(status));
+    console.log("STATUS : "+JSON.stringify(status));
 
     document.getElementById("cpuTemp").innerHTML = status.cpuTemp;
 
