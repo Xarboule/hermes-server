9517c4c31a2d4a39b185f56418c9453cf8c5b036
Remi Dulong <remi.dulong@telecom-sudparis.eu>
2017-06-08 14:39:43 +0200

    suite mode auto

--- client/controllers/automaticController.js
+++ client/controllers/automaticController.js
@@ -4,15 +4,23 @@
 
 var canvas = new fabric.Canvas('map');
 
-canvas.removeListeners();
+//canvas.removeListeners();
 canvas.backgroundColor = 'white';
+
+
 
 $.getJSON("/map/map.json", function(map){
    canvas.setHeight(map.map.y);
    canvas.setWidth(map.map.x);
 
-
-
+    canvas.add(new fabric.Circle({
+        originX: 'center',
+        originY: 'center',
+        left: map.map.xstart,
+        top: map.map.ystart,
+        fill: 'yellow',
+        radius: 50
+    }));
 
    for (var index in map.obstacles){
        var obstacle = map.obstacles[index];
@@ -22,7 +30,7 @@ $.getJSON("/map/map.json", function(map){
                 originY: 'center',
                 left: obstacle.xcenter,
                 top: obstacle.ycenter,
-                fill: 'red',
+                fill: 'darkred',
                 radius: obstacle.rayon
             }));
        }
@@ -33,7 +41,7 @@ $.getJSON("/map/map.json", function(map){
                originY: 'center',
                left: obstacle.xcenter,
                top: obstacle.ycenter,
-               fill: 'red',
+               fill: 'darkred',
                width: obstacle.width,
                height: obstacle.height,
                angle: (obstacle.angle/(2*Math.PI))*360
@@ -44,14 +52,35 @@ $.getJSON("/map/map.json", function(map){
        }
    }
 
-    canvas.add(new fabric.Triangle({
+    /*canvas.add(new fabric.Triangle({
         originX: 'center',
         originY: 'center',
         left: map.map.xstart,
         top: map.map.ystart,
         angle: map.map.anglestart,
         fill: 'green'
+    }));*/
+
+    fabric.Image.fromURL('/img/robot.png', function(oImg){
+        oImg.set({originX: 'center',
+            originY: 'center'});
+        oImg.set({'left': map.map.xstart});
+        oImg.set({'top': map.map.ystart});
+        oImg.set({'angle': map.map.anglestart/(2*Math.PI)*360});
+        oImg.scale(0.05);
+        canvas.add(oImg);
+
+    });
+
+    canvas.add(new fabric.Circle({
+        originX: 'center',
+        originY: 'center',
+        left: map.map.xstart,
+        top: map.map.ystart,
+        fill: 'green',
+        radius: 15
     }));
 
 });
+
 
